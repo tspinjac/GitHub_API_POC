17adca908be5664ac728f35f539152af24b9fdbc
Tomislav <tspinjac@gmail.com>
2018-01-24 23:20:53 +0100

    + Poboljšano cachiranje (osvježava User listu) + Dodan klik na usera u User listi kreira userDetails div + Mali refactoring u funkcije...

--- js/dinamic.js
+++ js/dinamic.js
@@ -1,4 +1,3 @@
-window.onload = function() {
 
 // 1.Kreiraj PARENT, container (uz dinamički css i postavljanje iznad skripte)
 var divContainer = document.createElement("div");
@@ -16,7 +15,25 @@ divHeader.setAttribute("style"," background-color: #24292e; color:white; text-al
 divHeader.innerHTML = "Github Users:"
 divContainer.appendChild(divHeader);//dodaj ga u container kao PRVO dijete
 
-//1.2: SECTION, drugo dijete containera (uz dinamički css)
+alert("onload");
+kreirajListu();
+
+
+
+
+function brisiListu()
+{
+    alert("brisem");
+    var temp = document.querySelector("section");
+    
+    temp.parentNode.removeChild(temp);
+}
+function kreirajListu(){
+//divContainer = document.querySelector(divContainer)
+divContainer = document.getElementById("container");
+alert(divContainer);
+alert("kreiram");
+  //1.2: SECTION, drugo dijete containera (uz dinamički css)
 var section = document.createElement("section");
 section.id = "section";
 section.setAttribute("style"," max-height:400px; max-width:300px; background-color: #e6eae6; padding:10px; overflow-y: scroll;");
@@ -29,7 +46,7 @@ nav.id = "nav";
 section.appendChild(nav); //Dodajemo OL u section
 
 //2. FETCH sa Googla https://developers.google.com/web/updates/2015/03/introduction-to-fetch
-fetch('https://api.github.com/users?page=2&per_page=100') //30 je default...
+fetch('https://api.github.com/users') //30 je default...
     .then(
         function(response) {
             if (response.status !== 200) {
@@ -63,10 +80,61 @@ fetch('https://api.github.com/users?page=2&per_page=100') //30 je default...
     )
     .catch(function(err) {
         console.log('Fetch Error :-S', err);
-    });
-
-}//KRAAAJJJ!!
-
-function mojafunkcija(parametar){
-localStorage.setItem(parametar, parametar);
+    });  
 }
+
+function mojafunkcija(parametar, document){
+localStorage.setItem(parametar, parametar);
+
+brisiListu();
+
+kreirajListu();
+
+userDetails(parametar);
+
+}
+//kreiramo div za user details
+function userDetails(parametar) {
+    var divContainer2 = window.document.createElement("div");
+    divContainer2.id="container2";
+    divContainer2.innerHTML = "Hello "
+    divContainer2.setAttribute("style"," margin:10px; max-height:700px; max-width:300px; border: 3px solid #73AD21; ");
+    window.document.body.appendChild(divContainer2);//dodaj ga u BODY
+
+  // var zatvoriX = window.document.createElement("p");
+
+alert("fetch");
+//pozvati api za details o useru
+fetch('https://api.github.com/users/'+parametar) //30 je default...
+    .then(
+        function(response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+            // Examine the text in the response for USERS via GITHUB API
+            response.json().then(function(data) {
+                console.log(data); //JSON sada imamo
+
+                //3.vrtimo Array i stvaramo za svaki element li, dajemo mu vrijednost logina i appendamo ga na RODITELJ - OL sa id "nav"
+              
+                
+                    
+                //CASCHIRAS SAMO PROMJENE; ako ne postoji ID usera u cachu, //var user = ;
+                    alert("BLALALALA");
+                    var listItem = window.document.createElement("div");
+                    listItem.innerHTML = "Repos:" + data.public_repos+".";
+                    window.document.body.appendChild(listItem); //li elemente smo dodati u nav ol! RADI! :D
+                
+                
+
+                
+            });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });  
+}
+
