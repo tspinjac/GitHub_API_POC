db4b63aef3a6afc23d09409e89573e5b222b8620
Tomislav <tspinjac@gmail.com>
2018-01-27 01:04:06 +0100

    # Final #

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# GitHub_API_POC
-Using Github Api to dynamicly load data on page
+# Kraken Junior FE Dev coding test - Proof of concept 
+Dynamicly creating DOM elements, fetching data via GitHub REST API, adding some styling.
+
+> 1. POC REPO: https://github.com/tspinjac/Junior-FE-Dev
+> 2. LIVE DEMO: https://tspinjac.github.io/Junior-FE-Dev/
+
+
+###Technology used:
+  * Plain (vanilla) Javascript
+  * Some basic CSS
+
+
+###Main Resources used:
+  * Google
+  * Stackoweflow.com
+  * MDN Web Docs - Mozilla
+  * Google Developers
+
+###TO DO (future...):
+  * code refactoring
+  * tweeks
+  * ...
+
+
+Tomislav Špinjača
--- js/dinamic.js
+++ js/dinamic.js
@@ -1,97 +1,60 @@
-/* */
-alert("Započni...");
+/* == For Kraken Systems miniproject, Tomislav == */
 
-/* ======================================= */
+
 createContainer();
 createList();
-/* ======================================= */
 
-
-/* ######## GLAVNE FUNKCIJE ######## */
-function createContainer () {
-
-   
+function createContainer () {   
     var createLoader = document.createElement("div");
         createLoader.id = "loader";
-        createLoader.setAttribute("style", "display:none; ");
-       
-       /*var createImg = document.createElement("div");
-            createImg.id = "loader";
-        createLoader.appendChild(createImg);*/
-
-    document.body.appendChild(createLoader);
-
+        createLoader.setAttribute("style", "display:none; "); 
+        document.body.appendChild(createLoader);
    
-    //1. Create and append divContainer; #container;  (dynamic CSS)
     var divContainer = document.createElement("div");
         divContainer.id = "container";
         divContainer.setAttribute("style", " margin:10px; max-height:700px; max-width:300px; border-radius: 5px; border: 3px solid #73AD21; ");
-        document.body.appendChild(divContainer); //dodaj ga u BODY
-        alert("Kreiran container div");
-    
-
-    //1.2. Create and append container HEADER; #header;  (dynamic CSS)
+        document.body.appendChild(divContainer); 
+       
     var divHeader = document.createElement("header");
         divHeader.id = "header";
-        divHeader.setAttribute("style", " background-color: #24292e; color:white; text-align: left; font-size:1.2em; padding:10px; ");
+        divHeader.setAttribute("style", " max-width:100%; background-color: #24292e; color:white; text-align: left; font-size:1.2em; padding:10px; ");
         divHeader.innerHTML = "Github Users:"
-        divContainer.appendChild(divHeader); //dodaj ga u container kao PRVO dijete
-/*
-    var btnX = document.createElement("span");
-        btnX.id = "btnX";
-        btnX.setAttribute("style", " position:absolute; top:0; right:5px; color:white; font-weight: bold; font-size:1.2em; ");
-        btnX.innerHTML = "&#10006;";
-        divHeader.appendChild(btnX); 
-        */
-        var btnId = "btnX";
-        kreirajX(btnId, divHeader);
+        divContainer.appendChild(divHeader); 
+
+        //
+    var btnId = "btnX";
+        createX(btnId, divHeader);
         document.querySelector("#"+btnId).addEventListener("click", hide);
         function hide(){document.querySelector("#container").style.display = "none";}
 }//END of createContainer
 
 function createList() {
-
-    //divContainer = document.querySelector("container") NE RADI???
     divContainer = document.getElementById("container");
-    //alert(divContainer); Što ovo radi?
-    alert("Dohvatio sam divContainer...slijedi kreiranje sectiona");
-
-    //1.3. Creating SECTION; #section; (dynamic CSS)
     var section = document.createElement("section");
     section.id = "section";
     section.setAttribute("style", " max-height:400px; max-width:300px; background-color: #e6eae6; padding:10px; overflow-y: scroll;");
-    section.innerHTML = ""; //placeholder
-    divContainer.appendChild(section); //dodaj ga u container kao DRUGO dijete
+    divContainer.appendChild(section); 
 
-    //1.4 Create OL (section first child); #nav; (no CSS)
     var nav = document.createElement("ol");
     nav.id = "nav";
     section.appendChild(nav); 
 
+    //Animacija: show animation during load ...
+    document.getElementById("loader").style.display = "block";
+                  
     //2. FETCH sa Googla https://developers.google.com/web/updates/2015/03/introduction-to-fetch
- document.getElementById("loader").style.display = "block";
-                    //loadingImg.style.width = "300px";
-                    //loadingImg.style.height = "300px";
-
     fetch('https://api.github.com/users') //30 je default...
         .then(
-
             function(response) {
-
-                   
-
                 if (response.status !== 200) {
                     console.log('Looks like there was a problem. Status Code: ' +
                         response.status);
                     return;
                 }
-           
-                  
 
                 response.json().then(function(data) {
                     console.log(data); //returns Javascript objects; key:"value"; (Array with js objects and I can work with objects :D) - not JSON OBJECT!
 
-                    //Loop Array
                     data.forEach(function(elementi) {
                         
                         //1. First check storage; 2. display all users which are not in storage; CACHING IDEA: caching changes only etc. clicked items (users)
@@ -99,17 +62,14 @@ function createList() {
                         if (storageContent == null) {
                             //If storage is empty, create li for all users...
                             var listItem = document.createElement("li"); 
-                            // <a onclick = mojafunkcija('elementi.login')> elementi.login </a>; event listener is better??
-                            listItem.innerHTML = "<a onclick=mojafunkcija('" + elementi.login + "') href=#>" + elementi.login + "</a>";
-                            nav.appendChild(listItem); //appending to parrent - nav :D
+                            // <a onclick = mainFunction('elementi.login')> elementi.login </a>; event listener is better...
+                            listItem.innerHTML = "<a onclick=mainFunction('" + elementi.login + "') href=#>" + elementi.login + "</a>";
+                            nav.appendChild(listItem); 
                         }
                     });
 
-                     document.getElementById("loader").style.display = "none";
-                        
-                    //Get and Check localstorage
-                    var storageContent = localStorage.getItem('');
-                    console.log("Numbers of items inside localstorage:", localStorage.length);
+                    //Hide loader animation when list is loaded
+                    document.getElementById("loader").style.display = "none";
                 });
             }
         )
@@ -118,22 +78,14 @@ function createList() {
         });
 }//END of createList
 
-
-
 //1. Delete old section
 function deleteList() {
-    alert("Deleting old user list...");
-    var deleteOldSection = document.querySelector("section");
+    //alert("Deleting old user list...");
+    var deleteOldSection = document.querySelector("#section");
     deleteOldSection.parentNode.removeChild(deleteOldSection);
 }//END of deleteList
 
-/*Main function:
-1. when user (li a) is clicked it stores key:value in cach (parametar, parametar)
-2. Deletes old list
-3. Creates new section and lists...
-4. Creates User datails view - fetch new api, get img, repos and gists
-*/
-function mojafunkcija(parametar) {
+function mainFunction(parametar) {
     localStorage.setItem(parametar, parametar);
     deleteList();
 
@@ -142,50 +94,36 @@ function mojafunkcija(parametar) {
     userDetails(parametar);
 
     document.querySelector("#container").style.display = "none";
-}//END of mojafunkcija
+}//END of mainFunction
 
-//4. Creating USER DETAILS VIEW...
+//Creating USER DETAILS VIEW...
 function userDetails(parametar) {
-    //Creating HTML elements...
+    
     var divContainer2 = window.document.createElement("div");
         divContainer2.id = "container2";
-        divContainer2.setAttribute("style", " margin: auto; width: 50%; border-radius: 5px; border: 3px solid green; margin:10px; max-height:700px; max-width:300px;");
-        window.document.body.appendChild(divContainer2); //dodaj ga u BODY
+        divContainer2.setAttribute("style", " margin: 0 auto; width: 50%; border-radius: 5px; border: 3px solid green; max-height:700px; max-width:300px;");
+        window.document.body.appendChild(divContainer2); 
 
     var divHeader2 = document.createElement("header");
         divHeader2.id = "header2";
         divHeader2.setAttribute("style", " position: relative; background-color: #24292E; color:white; text-align: left; font-size:1.2em; padding:10px;");
         divHeader2.innerHTML = "User Details: " + parametar;
-        divContainer2.appendChild(divHeader2); //first child
+        divContainer2.appendChild(divHeader2); 
 
     var userCompany = document.createElement("p");
         userCompany.id = "userCompany";
         userCompany.setAttribute("style", "text-alighn: center; font-size:1.2em; ");
-        //divContainer2.appendChild(userCompany);  company:undefined
-/*
-    var btnX2 = document.createElement("span");
-        btnX2.id = "btnX2";
-        btnX2.setAttribute("style", " position:absolute; top:0; right:5px; color:red; font-weight: bold; font-size:1.2em; ");
-        btnX2.innerHTML = "&#10006;";
-        divHeader2.appendChild(btnX2); 
-        */
-        var btn2Id = "btnX2"
-        kreirajX(btn2Id, divHeader2);
-        document.getElementById(btn2Id).addEventListener("click", obrisiX);
-        //function hide(){document.querySelector("#container2").style.display = "none";
+  
+    
+    var btn2Id = "btnX2"
+    createX(btn2Id, divHeader2);
+    document.getElementById(btn2Id).addEventListener("click", deleteDetails);
 
-        //var deleteOldSection = document.querySelector("section");
-        function obrisiX(){window.document.body.removeChild(document.getElementById("container2"));
-        document.querySelector("#container").style.display = "block";
-        //deleteOldSection.parentNode.removeChild(deleteOldSection);
-
-    }
-
-
-
-    //Getting data with API
-    alert("Fetch user details...");
-    //pozvati api za details o useru i ostalo...
+function deleteDetails(){
+    window.document.body.removeChild(document.getElementById("container2"));
+    document.querySelector("#container").style.display = "block";
+    }   
+    //alert("Fetch user details...");
     document.getElementById("loader").style.display = "block";
     fetch('https://api.github.com/users/' + parametar) //30 je default...
         .then(
@@ -198,9 +136,7 @@ function userDetails(parametar) {
                 
                 response.json().then(function(data) {
                     console.log("User details: ", data); 
-                    //Creating section and display data...
                     var userDetailsSection = window.document.createElement("section");
-                    //userCompany.innerText = data.avatar_url;
                     userDetailsSection.innerHTML = "<img src=" + data.avatar_url + ">" + "<br>"
                                                     + "<p> Company: " 
                                                     + data.company + "</p><br>"
@@ -216,14 +152,11 @@ function userDetails(parametar) {
         });
 }//END of userDetails
 
-
-function kreirajX(btnId, header){
-
+function createX(btnId, header){
     var btnX = document.createElement("span");
         btnX.id = btnId;
         btnX.setAttribute("style", " position:absolute; top:0; right:5px; color:white; font-weight: bold; font-size:1.2em; ");
         btnX.innerHTML = "&#10006;";
         header.appendChild(btnX); 
-
 }
 
